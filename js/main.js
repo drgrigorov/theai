79c934a0e11124486af1e44ab3d49c9759c1fd9f
Dimo Grigorov <drgrigorov@gmail.com>
2018-12-04 11:36:03 +0200

    Moved test branch files to js directory

--- js/main.js
+++ js/main.js
@@ -2,23 +2,26 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleMaintainer = require('role.maint');
+var roleMiner = require('role.miner');
+var roleMule = require('role.mule');
+
+var reg = require('screep.register');
 
 
 module.exports.loop = function () {
 
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
-        }
-    }
+    reg.check(Game.creeps, Memory.creeps);
 
-    var hsNum = 0, upNum = 0, buNum = 0, mnNum = 0, sTotNum = 0;
+    var hsNum = 0, muNum = 0, upNum = 0, buNum = 0, mnNum = 0, sTotNum = 0;
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
+        if(creep.memory.role == 'miner') {
+            roleMiner.run(creep);
             hsNum++;
+        }
+        if(creep.memory.role == 'carrier') {
+            roleMule.run(creep);
+            muNum++;
         }
         if(creep.memory.role == 'maintainer') {
             roleMaintainer.run(creep);
@@ -36,53 +39,60 @@ module.exports.loop = function () {
     }
     //console.log("Hs:", hsNum, " Up:", upNum, " Bu:", buNum);
 
-    if(hsNum < 2) {
-        var newName = 'Harvester' + Memory.nextSrc;
-        //console.log('Spawning new harvester: ' + newName);
-        if (Game.spawns['QStart'].spawnCreep([WORK,CARRY,MOVE,MOVE], newName,
-            {memory: {role: 'harvester', src: Memory.nextSrc % 2}}))
-        {
-            Memory.nextSrc++;
-        }
+    var mySpawn = Game.spawns['Spawn1'];
+    if(hsNum < reg.cfg.miner.cnt) {
+        var newName = 'Pump0';
+        mySpawn.spawnCreep(
+            reg.cfg.miner.parts,
+            'Pump0',
+            {memory: {role: reg.pers.Pump0.role, src: reg.pers.Pump0.src}} );
     }
-    
-    if(mnNum < 1) {
+    if(muNum < reg.cfg.carrier.cnt) {
+        var newName = 'Mule0';
+        console.log('Need to spawn carrier')
+        mySpawn.spawnCreep(
+            reg.cfg.carrier.parts,
+            'Mule1',
+            {memory: {role: reg.pers.Mule0.role}} );
+    }
+
+    if(mnNum < 0) {
         var newName = 'Maint' + Memory.nextSrc;
         //console.log('Spawning new harvester: ' + newName);
-        if (Game.spawns['QStart'].spawnCreep([WORK,CARRY,MOVE], newName,
+        if (mySpawn.spawnCreep([WORK,CARRY,MOVE], newName,
             {memory: {role: 'maintainer', src: Memory.nextSrc % 2}}))
         {
             Memory.nextSrc++;
         }
     }
-    
-    if(upNum < 1) {
+
+    if(upNum < 0) {
         var newName = 'Upgrader' + Memory.nextSrc;
         //console.log('Spawning new harvester: ' + newName);
-        if (Game.spawns['QStart'].spawnCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], newName,
+        if (mySpawn.spawnCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], newName,
             {memory: {role: 'upgrader', src: Memory.nextSrc % 2}}))
         {
             Memory.nextSrc++;
         }
     }
-    
-    if(buNum < 1) {
+
+    if(buNum < 0) {
         var newName = 'Builder' + Memory.nextSrc;
-        if (Game.spawns['QStart'].spawnCreep([WORK,WORK,CARRY,MOVE,MOVE,MOVE], newName,
+        if (mySpawn.spawnCreep([WORK,WORK,CARRY,MOVE,MOVE,MOVE], newName,
             {memory: {role: 'builder', src: Memory.nextSrc % 2}}))
         {
              Memory.nextSrc++;
         }
     }
 
-    if(Game.spawns['QStart'].spawning) {
+    if(mySpawn.spawning) {
         var spawningCreep = Game.creeps[Game.spawns['QStart'].spawning.name];
-        Game.spawns['QStart'].room.visual.text(
+        mySpawn.room.visual.text(
             '�️' + spawningCreep.memory.role,
             Game.spawns['QStart'].pos.x + 1,
             Game.spawns['QStart'].pos.y,
             {align: 'left', opacity: 0.8});
     }
 
-    
+
 }
