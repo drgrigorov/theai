cd1fd320fc976a0ccd5a17c513cb0ad6e78fa1d9
Dimo Grigorov <drgrigorov@gmail.com>
2019-01-03 19:06:01 +0200

    Gruntfile gets branch option

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,14 @@
 module.exports = function(grunt) {
 	var config = require('./.cred.json');
     grunt.loadNpmTasks('grunt-screeps');
+	var branch = grunt.option('branch') || config.branch;
 
     grunt.initConfig({
         screeps: {
             options: {
                 email: config.email,
                 password: config.password,
-                branch: config.branch,
+                branch: branch,
                 ptr: config.ptr
             },
             dist: {
